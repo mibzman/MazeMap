ff4fcab78d0a412a2c0340e2413747d364324858
mibzmam <27thisotope@gmail.com>
2015-03-02 20:19:59 -0500

    added all activities

---
+++ src/com/example/myapp/Trackingstopped.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sam on 3/2/2015.
+ */
+public class Trackingstopped extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trackingstopped);
+    }
+}
