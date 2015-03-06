0eda58d5bf1090eda570110c60d454b62ba06727
nodaskrub <nodaskrub@gmail.com>
2015-03-06 13:10:54 -0500

    PM: arrowscreen xml "Map" opens mapscreen activity

--- src/com/example/myapp/arrowscreen.java
+++ src/com/example/myapp/arrowscreen.java
@@ -1,7 +1,10 @@
 package com.example.myapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Sam on 3/2/2015.
@@ -10,5 +13,17 @@ public class arrowscreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arrowscreen);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendIntent();
+            }
+        });
+
+    }
+    public void sendIntent(){
+        Intent intent = new Intent(this, Mapscreen.class);
+        startActivity(intent);
     }
 }
