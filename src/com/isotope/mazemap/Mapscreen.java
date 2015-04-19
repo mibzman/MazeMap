d70ceeb848da9354358fd632ff0673fbfba45f80
mibzmam <27thisotope@gmail.com>
2015-04-19 19:25:29 -0400

    added animation test

--- src/com/isotope/mazemap/Mapscreen.java
+++ src/com/isotope/mazemap/Mapscreen.java
@@ -3,17 +3,65 @@ package com.isotope.mazemap;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Sam on 3/2/2015.
  */
 public class Mapscreen extends Fragment {
+    float oldX = 0;
+    float oldY = 0;
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle args) {
-        View view = inflater.inflate(R.layout.mapscreen, container, false);
+        final View view = inflater.inflate(R.layout.mapscreen, container, false);
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstAnimation(view);
+
+            }
+        });
         return view;
+    }
+    void firstAnimation(final View view){
+        View animatedView = view.findViewById(R.id.theDot);
+        final float x = -50;
+        final float y =0;
+        TranslateAnimation animation = new TranslateAnimation(
+                oldX, (x + oldX), oldY, (y +oldY)
+        );
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
+        animatedView.startAnimation(animation);
+        oldX = (x+oldX);
+        oldY = (x+oldY);
+        //secondAnimation(view);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                secondAnimation(view);
+            }
+        }, 1000);
+    }
+    void secondAnimation(View view){
+        View animatedView = view.findViewById(R.id.theDot);
+        final float x = 0;
+        final float y =-100;
+        TranslateAnimation animation = new TranslateAnimation(
+                oldX, (x + oldX), oldY, (y +oldY)
+        );
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
+        animatedView.startAnimation(animation);
+        oldX = (x+oldX);
+        oldY = (x+oldY);
     }
 }
--- src/com/isotope/mazemap/MyActivity.java
+++ src/com/isotope/mazemap/MyActivity.java
@@ -21,36 +21,47 @@ public class MyActivity extends Activity {
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bestmain);
+        navdrawergarbage();
+    Fragment Map = new Mapscreen();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
+
+        }
+    void navdrawergarbage() {
         menu = new String[]{"Navigate","Tips", "Settings"};
         dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         dList = (ListView) findViewById(R.id.left_drawer);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
-
         dList.setAdapter(adapter);
         dList.setSelector(android.R.color.holo_blue_dark);
         dList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
-@Override
-public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
-        dLayout.closeDrawers();
-        //args.putString("Menu", menu[position]);
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+                dLayout.closeDrawers();
+                if(position == 0) {
+                    Fragment Map = new Mapscreen();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
 
-        if(position == 0) {
-        Fragment Map = new Mapscreen();
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
-        }else if (position== 1){
-        Fragment Tips = new Tips();
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, Tips).commit();
-        }else if (position== 2){
-            Fragment Settings = new Settings();
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_frame, Settings).commit();
-        }
-        }
+                }else if (position== 1){
+                    Fragment Tips = new Tips();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content_frame, Tips).commit();
+                }else if (position== 2){
+                    Fragment Settings = new Settings();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content_frame, Settings).commit();
+                }else{
+                    Fragment Map = new Mapscreen();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
+                    dList.setItemChecked(0, true);
+                    dList.setSelection(0);
+                }
+            }
 
         });
-        }
+    }
 
 }
