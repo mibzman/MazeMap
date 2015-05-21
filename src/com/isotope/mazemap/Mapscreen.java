7f2a234bdd0376df8e01d6a7a08ede44fa43fa02
mibzmam <27thisotope@gmail.com>
2015-05-21 16:58:15 -0400

    added images (including thenewdot)

--- src/com/isotope/mazemap/Mapscreen.java
+++ src/com/isotope/mazemap/Mapscreen.java
@@ -2,6 +2,13 @@ package com.isotope.mazemap;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -10,17 +17,30 @@ import android.view.ViewGroup;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Sam on 3/2/2015.
  */
-public class Mapscreen extends Fragment {
+public class Mapscreen extends Fragment{
+    //location at paul's seat: Lat:41.4846039, Long:-81.7835846
+   // LocationManager locationManager;
+    //LocationListener locationListener;
+
     float oldX = 0;
     float oldY = 0;
+    int currentLat;
+    int currentLong;
+    int oldLat;
+    int oldLong;
+    Context context;
+    double Lat1 = 41.4906377;
+    double oldLon = -81.056922;
+    LocationManager locationManager;
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle args) {
         final View view = inflater.inflate(R.layout.mapscreen, container, false);
-        Button button = (Button) view.findViewById(R.id.button);
+        Button button = (Button) view.findViewById(R.id.Start);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +48,40 @@ public class Mapscreen extends Fragment {
 
             }
         });
+
         return view;
+
+/*
+        //location start
+
+        //String context = Context.LOCATION_SERVICE;
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        //locationManager = (LocationManager) getActivity.getSystemService(context);
+
+        Criteria crta = new Criteria();
+        crta.setAccuracy(Criteria.ACCURACY_FINE);
+        crta.setAltitudeRequired(false);
+        crta.setBearingRequired(false);
+        crta.setCostAllowed(true);
+        crta.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(crta, true);
+
+        // String provider = LocationManager.GPS_PROVIDER;
+        final Location location = locationManager.getLastKnownLocation(provider);
+        updateWithNewLocation(location);
+
+        locationManager.requestLocationUpdates(provider, 1000, 0,
+                locationListener);
+        Button mbutton = (Button) view.findViewById(R.id.Start);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Lat1 = location.getLatitude();
+                oldLon = location.getLongitude();
+            }
+        });
+//location end
+*/
     }
     void firstAnimation(final View view){
         View animatedView = view.findViewById(R.id.theDot);
@@ -63,5 +116,76 @@ public class Mapscreen extends Fragment {
         animatedView.startAnimation(animation);
         oldX = (x+oldX);
         oldY = (x+oldY);
+    }
+    private final LocationListener locationListener = new LocationListener()
+    {
+
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            updateWithNewLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+            updateWithNewLocation(null);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+        }
+
+    };
+
+    private void updateWithNewLocation(Location location)
+    {
+        String latLong;
+        TextView myLocation;
+       // myLocation = (TextView) findViewById(R.id.textView);
+        if (location != null)
+        {
+            //this is all from http://www.movable-type.co.uk/scripts/latlong.html
+            double lat2 = location.getLatitude();
+            double lon = location.getLongitude();
+            latLong = "Lat:" + lat2 + "\nLong:" + lon;
+            double R = 6371000;
+            double sigma1 = Math.toRadians(Lat1);
+            double sigma2 = Math.toRadians(lat2);
+            //double deltaSigma = Math.toRadians()
+
+            double radOldlat = Math.toRadians(Lat1);
+            double radOldlon = Math.toRadians(oldLon);
+
+            double radLat = Math.toRadians(location.getLatitude());
+            double radLon = Math.toRadians(location.getLongitude());
+
+
+            double deltaSigma = Math.toRadians((radOldlat - radLat));
+            double deltaLambda = Math.toRadians((radOldlon - radLon));
+
+            double a = (Math.sin(deltaSigma/2)*Math.sin(deltaSigma)) +
+                    Math.cos(sigma1)*Math.cos(sigma2)*
+                            (Math.sin(deltaLambda/2)*Math.sin(deltaLambda/2));
+
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            double d = R * c;
+            final String DeeString = Double.toString(d);
+            //setTheText(DeeString);
+            //oldLat = lat;
+            //oldLon = lon;
+        } else{
+            latLong = " NO Location Found ";
+        }
+        //  TextView theTextView = (TextView) findViewById(R.id.butt);
+        //theTextView.setText(DeeString);
+       // myLocation.setText("your Current Position is :\n" + latLong);
     }
 }
--- src/com/isotope/mazemap/MyActivity.java
+++ src/com/isotope/mazemap/MyActivity.java
@@ -1,15 +1,18 @@
 package com.isotope.mazemap;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class MyActivity extends Activity {
     String[] menu;
@@ -17,14 +20,17 @@ public class MyActivity extends Activity {
     ListView dList;
     ArrayAdapter<String> adapter;
 
+
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bestmain);
-        navdrawergarbage();
+    //nav drawer start
+    navdrawergarbage();
     Fragment Map = new Mapscreen();
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
+//nav drawer end
 
         }
     void navdrawergarbage() {
@@ -34,6 +40,11 @@ protected void onCreate(Bundle savedInstanceState) {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
         dList.setAdapter(adapter);
         dList.setSelector(android.R.color.holo_blue_dark);
+        Fragment Map = new Mapscreen();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, Map).commit();
+        dList.setItemChecked(0, true);
+        dList.setSelection(0);
         dList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
